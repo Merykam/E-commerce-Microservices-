16f3c229cc2093fe619827624dbb749fa76c5c8a
rachiddaoudi <rachiddaoudi533@gmail.com>
2023-12-27 23:51:17 +0100

    realiser crud de produit

--- apps/service-cart/src/product/entities/product.entity.ts
+++ apps/service-cart/src/product/entities/product.entity.ts
@@ -2,6 +2,7 @@ export class Product {
   name: string;
   price: number;
   description: string;
+  quantity: number;
   userId: number;
   categoryId: number;
   createdAt: Date;
--- apps/service-cart/src/product/product.service.ts
+++ apps/service-cart/src/product/product.service.ts
@@ -1,26 +1,94 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from '../config/config.service';
 
 @Injectable()
 export class ProductService {
+  constructor(private readonly prismaSevice: PrismaService) {}
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    try {
+      const product = this.prismaSevice.product.create({
+        data: {
+          name: createProductDto.name,
+          image: createProductDto.image,
+          price: createProductDto.price,
+          description: createProductDto.description,
+          quantity: createProductDto.quantity,
+          userId: createProductDto.userId,
+          categoryId: createProductDto.categoryId,
+          createdAt: createProductDto.createdAt,
+          updatedAt: createProductDto.updatedAt,
+        },
+      });
+
+      return product;
+    } catch (error) {
+      throw new Error('Failed to create product');
+    }
   }
 
   findAll() {
-    return `This action returns all product`;
+    try {
+      const products = this.prismaSevice.product.findMany();
+      return products;
+    } catch (error) {
+      throw new Error('Failed to find products');
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    try {
+      const product = await this.prismaSevice.product.findUnique({
+        where: {
+          id: id,
+        },
+      });
+
+      if (!product) {
+        return 'product not found';
+      }
+
+      return product;
+    } catch (error) {
+      throw new Error('Failed to find products');
+    }
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+    try {
+      const product = this.prismaSevice.product.update({
+        where: {
+          id: id,
+        },
+        data: {
+          name: updateProductDto.name,
+          image: updateProductDto.image,
+          price: updateProductDto.price,
+          description: updateProductDto.description,
+          quantity: updateProductDto.quantity,
+          userId: updateProductDto.userId,
+          categoryId: updateProductDto.categoryId,
+          createdAt: updateProductDto.createdAt,
+          updatedAt: updateProductDto.updatedAt,
+        },
+      });
+
+      return product;
+    } catch (error) {}
   }
 
   remove(id: number) {
-    return `This action removes a #${id} product`;
+    try {
+      const product = this.prismaSevice.product.delete({
+        where: {
+          id: id,
+        },
+      });
+
+      return product;
+    } catch (error) {
+      throw new Error('Failed to delete product');
+    }
   }
 }
