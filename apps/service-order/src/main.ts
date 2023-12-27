815df7fbc87e5cf3801eac1bb29c2b7149684831
Ossama kharbaq <ossamakharbaq4@gmail.com>
2023-12-27 18:41:08 +0100

    create a global prisma class

--- apps/service-order/src/main.ts
+++ apps/service-order/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { ServiceOrderModule } from './service-order.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(ServiceOrderModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(8080);
 }
 bootstrap();
--- apps/service-order/src/service-order.module.ts
+++ apps/service-order/src/service-order.module.ts
@@ -3,8 +3,15 @@ import { UserModule } from './user/user.module';
 import { ProductModule } from './product/product.module';
 import { OrderItemModule } from './order-item/order-item.module';
 import { OrderModule } from './order/order.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports : [UserModule,ProductModule, OrderItemModule, OrderModule]
+  imports: [
+    UserModule,
+    ProductModule,
+    OrderItemModule,
+    OrderModule,
+    PrismaModule,
+  ],
 })
 export class ServiceOrderModule {}
