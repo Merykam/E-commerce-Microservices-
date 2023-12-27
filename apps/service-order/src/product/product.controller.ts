0ecb16219a00e1145a96cb247f5f800bdd1ff68b
Ossama kharbaq <ossamakharbaq4@gmail.com>
2023-12-27 18:39:51 +0100

    finish the v1 of the product crud

---
+++ apps/service-order/src/product/product.controller.ts
@@ -0,0 +1,33 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+
+import { ProductService } from './product.service';
+import { ProductDTO } from './DTO';
+
+@Controller('product')
+export class ProductController {
+  constructor(private ProductService: ProductService) {}
+
+  @Post('')
+  createProduct(@Body() data: ProductDTO) {
+    console.log(data);
+    return this.ProductService.createProduct(data);
+  }
+
+  @Patch(':id')
+  updateProduct(@Param('id') id: string, @Body() data: ProductDTO) {
+    return this.ProductService.updateProduct(id, data);
+  }
+
+  @Delete(':id')
+  deleteProduct(@Param('id') id: string) {
+    return this.ProductService.deleteProduct(id);
+  }
+}
