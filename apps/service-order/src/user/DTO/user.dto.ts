cebb34636763951974b292f1b167052f04605192
Ossama kharbaq <ossamakharbaq4@gmail.com>
2023-12-28 16:04:29 +0100

    handle the error in the user crud

--- apps/service-order/src/user/DTO/user.dto.ts
+++ apps/service-order/src/user/DTO/user.dto.ts
@@ -1,15 +1,15 @@
 import { IsEmail, IsInt, IsNotEmpty, IsString } from 'class-validator';
 
 export class UserDTO {
-  @IsInt()
-  @IsNotEmpty()
+  @IsInt({ message: 'id should be an integer' })
+  @IsNotEmpty({ message: 'id is required' })
   id: number;
 
   @IsEmail()
-  @IsNotEmpty()
+  @IsNotEmpty({message : "Email is required"})
   email: string;
 
-  @IsString()
-  @IsNotEmpty()
+  @IsString({message : "name should be a string "})
+  @IsNotEmpty({message : "Name is required"})
   name: string;
 }
--- apps/service-order/src/user/user.service.ts
+++ apps/service-order/src/user/user.service.ts
@@ -1,31 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { UserDTO } from './DTO';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
   async createUser(data: UserDTO) {
-    const insertedUser = await this.prisma.user.create({ data });
-    return insertedUser;
+    try {
+      const insertedUser = await this.prisma.user.create({ data });
+      return insertedUser;
+    } catch (error) {
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      )
+        throw new ForbiddenException('Credentials taken');
+      throw error;
+    }
   }
 
   async updateUser(id: number, data: UserDTO) {
-    const updatedUser = await this.prisma.user.update({
-      where: { id },
-      data : {
-        email : data.email,
-        name: data.name
-      },
-    });
-    return updatedUser
+    try {
+      const updatedUser = await this.prisma.user.update({
+        where: { id },
+        data: {
+          email: data.email,
+          name: data.name,
+        },
+      });
+      return updatedUser;
+    } catch (error) {
+      if (
+        error instanceof PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      )
+        throw new ForbiddenException('Credentials taken');
+      throw error;
+    }
   }
 
   async deleteUser(id: number) {
     const deletedUser = await this.prisma.user.delete({
-      where: { id }
+      where: { id },
     });
-    return deletedUser
+    return deletedUser;
   }
 }
