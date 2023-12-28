ad93a5e2e4c086df1aa3e07c1c86e4542a7851a0
Ossama kharbaq <ossamakharbaq4@gmail.com>
2023-12-28 16:00:54 +0100

    create the order item service

---
+++ apps/service-order/src/order-item/order-item.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { OrderItemDTO } from './DTO';
+
+@Injectable()
+export class OrderItemService {
+  constructor(private prisma: PrismaService) {}
+
+  async createOrderItem(data: OrderItemDTO) {
+    const insertedOrderItem = await this.prisma.orderItem.create({data});
+    return insertedOrderItem;
+  }
+
+  async updateOrderItem(id: number, data: OrderItemDTO) {
+    const updatedOrderItem = await this.prisma.orderItem.update({
+      where: { id },
+      data: {
+        quantity: data.quantity,
+      },
+    });
+    return updatedOrderItem;
+  }
+
+  async deleteOrderItem(id: number) {
+    const deletedOrderItem = await this.prisma.orderItem.delete({
+      where: { id },
+    });
+    return deletedOrderItem;
+  }
+}
