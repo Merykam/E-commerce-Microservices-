e681739c7c7f7532c823d6204e91ffdf66b8b470
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-01-01 15:43:24 +0100

    implement the order creation and update

--- apps/service-order/src/order/order.controller.ts
+++ apps/service-order/src/order/order.controller.ts
@@ -1,10 +1,21 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
-import { OrderDTO } from './DTO';
+import { Body, Controller, Get, Patch, Post, UsePipes } from '@nestjs/common';
+import { OrderDTO, UpdateOrderDTO } from './DTO';
+import { OrderService } from './order.service';
+import { OrderItemsValidationPipe } from './pipes/order-items-validation.pipe';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Controller('order')
 export class OrderController {
-    @Post('')
-    createOrder(@Body() data: OrderDTO){
-        return data
-    }
+  constructor(private OrderService: OrderService) {}
+
+  @Post('')
+  @UsePipes(new OrderItemsValidationPipe(new PrismaService))
+  createOrder(@Body() data: OrderDTO) {
+    return this.OrderService.createOrder(data);
+  }
+
+  @Patch('')
+  updateOrder(@Body() data: UpdateOrderDTO) {
+    return this.OrderService.updateOrder(data);
+  }
 }
