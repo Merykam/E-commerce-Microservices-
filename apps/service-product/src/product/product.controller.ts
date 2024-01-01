b3809d3bb195c2c370098f0c69784fca8591e05f
mariem kamaych <kamaychmeryem@gmail.com>
2024-01-01 17:40:11 +0100

    findProduct by id function

--- apps/service-product/src/product/product.controller.ts
+++ apps/service-product/src/product/product.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { Product } from './schemas/product.schema';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -20,7 +20,14 @@ export class ProductController {
     product:CreateProductDto,): Promise<Product>{
         return this.productService.create(product)
     }
-    
+    @Get(':id')
+    async getProduct(@Param('id')
+    id: string): Promise<Product>{
+        const product =  this.productService.findById(id)
+
+        return product
+    }
+
 
     // @Post('new')
     // async createProduct(@Body() createProductDto: CreateProductDto): Promise<Product> {
--- apps/service-product/src/product/product.service.ts
+++ apps/service-product/src/product/product.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Product } from './schemas/product.schema';
 import * as mongoose from 'mongoose';
@@ -18,5 +18,14 @@ export class ProductService {
     async create(product:CreateProductDto): Promise<Product>{
         const res = await this.productModel.create(product)
         return res;
+    }
+
+    async findById(id: string): Promise<Product>{
+        const product = await this.productModel.findById(id);
+        if(!product){
+            throw new NotFoundException('Product not found')
+        }
+        return product;
+
     }
 }
