3283b8a06a2ef8ee6b476fb505d6f1709f4de9dd
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-01-02 11:48:54 +0100

    change the update url

--- apps/service-order/src/order/order.controller.ts
+++ apps/service-order/src/order/order.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Patch, Post, UsePipes } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, Post, UsePipes } from '@nestjs/common';
 import { OrderDTO, UpdateOrderDTO } from './DTO';
 import { OrderService } from './order.service';
 import { OrderItemsValidationPipe } from './pipes/order-items-validation.pipe';
@@ -8,14 +8,19 @@ import { PrismaService } from '../prisma/prisma.service';
 export class OrderController {
   constructor(private OrderService: OrderService) {}
 
+  @Get('')
+  getOrders() {
+    return this.OrderService.getAllOrders();
+  }
+
   @Post('')
   @UsePipes(new OrderItemsValidationPipe(new PrismaService))
   createOrder(@Body() data: OrderDTO) {
     return this.OrderService.createOrder(data);
   }
 
-  @Patch('')
-  updateOrder(@Body() data: UpdateOrderDTO) {
-    return this.OrderService.updateOrder(data);
+  @Patch(':id')
+  updateOrder(@Param('id') id: string , @Body() data: UpdateOrderDTO) {
+    return this.OrderService.updateOrder(id ,data);
   }
 }
