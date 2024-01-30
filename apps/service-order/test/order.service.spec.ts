ddd5a1e69c1b16d571667a66ca0e427f86b9f0da
Ossama kharbaq <ossamakharbaq4@gmail.com>
2024-01-30 09:58:20 +0100

    test the createOrder functionality

--- apps/service-order/test/order.service.spec.ts
+++ apps/service-order/test/order.service.spec.ts
@@ -51,5 +51,45 @@ describe('OrderService', () => {
       expect(prismaService.order.findMany).toHaveBeenCalled();
     });
   });
+  describe('updateOrder', () => {
+    it('should update an existing order', async () => {
+      const updateOrderDTO: UpdateOrderDTO = {
+        status: Status.value2,
+        paid: true,
+      };
+      const updatedOrder = {
+        id: 1,
+        userId: 1,
+        status: 'In Delivery',
+        paid: true,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+      jest.spyOn(prismaService.order, 'update').mockResolvedValue(updatedOrder);
+
+      const result = await orderService.updateOrder('1', updateOrderDTO);
+
+      expect(result).toEqual(updatedOrder);
+      expect(prismaService.order.update).toHaveBeenCalledWith({
+        where: { id: 1 },
+        data: updateOrderDTO,
+      });
+    });
+
+    it('should throw an error if updating the order fails', async () => {
+      const updateOrderDTO: UpdateOrderDTO = {
+        status: Status.value2,
+        paid: true,
+      };
+      jest
+        .spyOn(prismaService.order, 'update')
+        .mockRejectedValue(new Error('Database error'));
+
+      await expect(
+        orderService.updateOrder('1', updateOrderDTO),
+      ).rejects.toThrow(InternalServerErrorException);
+      expect(prismaService.order.update).toHaveBeenCalled();
+    });
+  });
   
 });
