4bdaa53861b3b5d76ce23410236030f90f6ac931
mariem kamaych <kamaychmeryem@gmail.com>
2024-01-30 10:45:42 +0100

    qlqs modifs

--- frontendecommerce/src/pages/dashboard/product/index.tsx
+++ frontendecommerce/src/pages/dashboard/product/index.tsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 
+
 const index = () => {
 
 
@@ -40,6 +41,7 @@ const index = () => {
 
   return (
     <div className="">
+    
    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/Loopple/loopple-public-assets@main/riva-dashboard-tailwind/riva-dashboard.css"/>
     <div className="flex flex-wrap mb-5  ">
@@ -143,7 +145,7 @@ const index = () => {
                               
                                   <button className="text-success  rounded p-1 m-1" >
                                   {/* <button className="text-success  rounded p-1 m-1" onClick={()=>console.log(appart?._id)}> */}
-                                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                                  <svg  xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                                     <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
                                   </svg>
                                   </button>
