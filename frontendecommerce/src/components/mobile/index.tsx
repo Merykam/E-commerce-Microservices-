80481079fad9bee5224699a96aaeace474c6e56e
rachiddaoudi <rachiddaoudi533@gmail.com>
2024-01-31 10:43:44 +0100

    réaliser page products

--- frontendecommerce/src/components/mobile/index.tsx
+++ frontendecommerce/src/components/mobile/index.tsx
@@ -1,4 +1,5 @@
 const mobile = () => {
+
   return (
     <>
       <div className="sticky top-0 inset-x-0 z-20 bg-white border-y px-4 sm:px-6 md:px-8 lg:hidden dark:bg-gray-800 dark:border-gray-700">
---
+++ frontendecommerce/src/pages/cart/index.tsx
@@ -0,0 +1,13 @@
+import LayoutHome from "@/components/layout/layoutHome";
+
+const Cart = () =>{
+    return(
+        <>
+        <LayoutHome>
+            <div></div>
+            </LayoutHome>
+        </>
+    )
+}
+
+export default Cart;
