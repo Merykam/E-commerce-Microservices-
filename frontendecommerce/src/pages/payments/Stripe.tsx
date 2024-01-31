7e1a5ecdecffea07d7c5eb3b7287b1351c193482
youssef tadlaoui <usftadlaoui@gmail.com>
2024-01-31 16:08:13 +0100

    add front for stripe

---
+++ frontendecommerce/src/pages/payments/Stripe.tsx
@@ -0,0 +1,34 @@
+import React, { useState } from 'react'
+import StripeCheckout, { Token } from 'react-stripe-checkout';
+const Stripe = () => {
+ const [product,setProduct] =useState({name:"React from FB",price:10,productBy:"facebook"})
+ const yy=process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY
+  console.log(yy)
+  const handleToken = (token: Token) => {
+  //  console.log('Received token:', token);
+  const body={
+    token,
+    product
+  }
+  const headers={
+    "Content-Type":"application/json"
+  }
+  return fetch(`http://localhost:3003/payment/stripe`,{
+    method:"POST",
+    headers,
+    body:JSON.stringify(body)
+  }).then(response=>{
+    console.log("RESPONSE",response)
+    const {status}=response
+    console.log("STATUS",status)
+  }).catch(error=>console.log(error))
+};
+  return (
+    <div>
+      Stripe
+      <StripeCheckout stripeKey={process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY!} token={handleToken} name='buy by react'/>
+    </div>
+  )
+}
+
+export default Stripe
